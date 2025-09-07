be0151199e6c228e113f176735963759e627b91e
chev <chevv0004@gmail.com>
2025-09-07 00:03:00 -0500

    feat: accept via username

--- src/main/java/com/chevvy/commands/teleport/TpaResponseCommand.java
+++ src/main/java/com/chevvy/commands/teleport/TpaResponseCommand.java
@@ -9,7 +9,6 @@ import net.minecraft.command.CommandSource;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 
 import java.util.*;
 import java.util.stream.Collectors;
